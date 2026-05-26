5cdd09778ab35bf48d8afe0e6067777cae3186c5
Timothy Hoffman <4001421+tim-hoffman@users.noreply.github.com>
2026-05-25 21:02:56 -0500

    Add tests for `ram` dialect and a few random fixes (#476)

--- backends/smt/include/smt/Conversions/ConversionPasses.h
+++ backends/smt/include/smt/Conversions/ConversionPasses.h
@@ -2,7 +2,7 @@
 //
 // Part of the LLZK Project, under the Apache License v2.0.
 // See LICENSE.txt for license information.
-// Copyright 2026 Project LLZK.
+// Copyright 2026 Project LLZK
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
