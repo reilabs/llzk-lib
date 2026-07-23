232367af29e6bcf50c04d7a15762535a4a256b63
krushimir <189111540+krushimir@users.noreply.github.com>
2026-07-23 18:22:02 +0200

    Allow compute operations during PCL conversion

--- backends/pcl/lib/Conversion/PCLLoweringPass.cpp
+++ backends/pcl/lib/Conversion/PCLLoweringPass.cpp
@@ -657,11 +657,7 @@ static bool isStep1LegalOp(Operation *op) {
 static void populateStep1ConversionTarget(ConversionTarget &target, NonDetOpNames &names) {
   target.addLegalDialect<pcl::PCLDialect, func::FuncDialect>();
   target.addLegalOp<ModuleOp, UnrealizedConversionCastOp>();
-  target.addDynamicallyLegalDialect<
-      BoolDialect, FeltDialect, CastDialect, arith::ArithDialect, ConstrainDialect,
-      array::ArrayDialect, global::GlobalDialect, include::IncludeDialect, pod::PODDialect,
-      polymorphic::PolymorphicDialect, ram::RAMDialect, smt::SMTDialect, string::StringDialect,
-      verif::VerifDialect, LLZKDialect, StructDialect, FunctionDialect>(isStep1LegalOp);
+  target.markUnknownOpDynamicallyLegal(isStep1LegalOp);
 
   target.addDynamicallyLegalOp<NonDetOp>([&names](NonDetOp op) {
     return isStep1LegalOp(op) && names.find(op) == names.end();
